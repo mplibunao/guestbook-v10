ec29f63fb13cf15bce8710dd4a7025d744c1886a
MP <markpaololibunao@gmail.com>
2022-12-12 08:47:48 +0800

    Done

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,11 +1,11 @@
 // src/server/trpc/router/_app.ts
 import { router } from "../trpc";
-import { exampleRouter } from "./example";
 import { authRouter } from "./auth";
+import { guestbookRouter } from "./guestbook";
 
 export const appRouter = router({
-  example: exampleRouter,
   auth: authRouter,
+  guestbook: guestbookRouter,
 });
 
 // export type definition of API
